da7e92d9f830ddc6745a6b8cf32c03b53fad2a2e
Eric Nwogbo <ericis2007@gmail.com>
2024-11-24 10:22:38 +0100

    Infix parselets source location

--- src/yoyo/parselets/infix_parselets.cpp
+++ src/yoyo/parselets/infix_parselets.cpp
@@ -8,22 +8,44 @@ namespace Yoyo
     std::unique_ptr<Expression> BinaryOperationParselet::parse(Parser& parser, std::unique_ptr<Expression> left, Token tk)
     {
         const auto prec_diff = is_right ? 1 : 0;
-        return std::make_unique<BinaryOperation>(tk, std::move(left), parser.parseExpression(prec - prec_diff));
+        auto fn = [&parser](std::unique_ptr<BinaryOperation>&& e)
+        {
+            e->lhs->parent = e.get(); e->rhs->parent = e.get(); e->parent = parser.parent;
+            e->beg = e->lhs->beg; e->end = e->rhs->end; return e;
+        };
+        return fn(std::make_unique<BinaryOperation>(tk, std::move(left), parser.parseExpression(prec - prec_diff)));
     }
     std::unique_ptr<Expression> LogicalOperationParselet::parse(Parser& parser, std::unique_ptr<Expression> left, Token tk)
     {
         const auto prec_diff = is_right ? 1 : 0;
-        return std::make_unique<LogicalOperation>(tk, std::move(left), parser.parseExpression(prec - prec_diff));
+        auto fn = [&parser](std::unique_ptr<LogicalOperation>&& e)
+        {
+            e->lhs->parent = e.get(); e->rhs->parent = e.get(); e->parent = parser.parent;
+            e->beg = e->lhs->beg; e->end = e->rhs->end; return e;
+        };
+        return fn(std::make_unique<LogicalOperation>(tk, std::move(left), parser.parseExpression(prec - prec_diff)));
     }
     std::unique_ptr<Expression> PostfixOperationParselet::parse(Parser& parser, std::unique_ptr<Expression> left, Token tk)
     {
-        return std::make_unique<PostfixOperation>(tk, std::move(left));
+        auto fn = [&parser](std::unique_ptr<PostfixOperation>&& e)
+        {
+            e->operand->parent = e.get(); e->parent = parser.parent;
+            e->beg = e->operand->beg; e->end = SourceLocation{e->beg.line, e->beg.column + e->op.text.size()};
+            return e;
+        };
+        return fn(std::make_unique<PostfixOperation>(tk, std::move(left)));
     }
     std::unique_ptr<Expression> CallOperationParselet::parse(Parser& parser, std::unique_ptr<Expression> left, Token tk)
     {
+        auto fn = [&parser](std::unique_ptr<CallOperation>&& e)
+        {
+            e->callee->parent = e.get(); e->parent = parser.parent;
+            for(auto& arg : e->arguments) arg->parent = e.get();
+            e->beg = e->callee->beg; e->end = parser.discardLocation; return e;
+        };
         std::vector<std::unique_ptr<Expression>> arguments;
         if(parser.discard(TokenType::RParen))
-            return std::make_unique<CallOperation>(std::move(left), std::move(arguments));
+            return fn(std::make_unique<CallOperation>(std::move(left), std::move(arguments)));
 
         arguments.push_back(parser.parseExpression(0));
         while(!parser.discard(TokenType::RParen))
@@ -32,7 +54,7 @@ namespace Yoyo
             auto arg = parser.parseExpression(0);
             arguments.push_back(std::move(arg));
         }
-        return std::make_unique<CallOperation>(std::move(left), std::move(arguments));
+        return fn(std::make_unique<CallOperation>(std::move(left), std::move(arguments)));
     }
 
     std::unique_ptr<Expression> ScopeOperationParselet::parse(Parser& parser, std::unique_ptr<Expression> left, Token tk)
@@ -58,9 +80,12 @@ namespace Yoyo
             if(expr_as_init)
             {
                 expr_as_init->t.name = std::string{as_name->text} + "::" + expr_as_init->t.name;
+                expr_as_init->beg = as_name->beg;
                 return expr;
             }
-            return std::make_unique<ScopeOperation>(Type{.name= std::string{as_name->text}}, "", std::string{expr_as_name->text});
+            return Expression::attachSLAndParent(
+                std::make_unique<ScopeOperation>(Type{.name= std::string{as_name->text}}, "", std::string{expr_as_name->text}),
+                as_name->beg, expr_as_name->end, parser.parent);
         }
         if(as_scope)
         {
@@ -68,11 +93,13 @@ namespace Yoyo
             {
                 std::string prefix = as_scope->scope.empty() ? "" : as_scope->scope + "::";
                 expr_as_init->t.name =  prefix + as_scope->type.name + "::" + as_scope->name + "::" + expr_as_init->t.name;
+                expr_as_init->beg = as_scope->beg;
                 return expr;
             }
             as_scope->scope += "::" + as_scope->type.name;
             as_scope->type.name = as_scope->name;
             as_scope->name = std::string{expr_as_name->text};
+            as_scope->end = expr_as_name->beg;
             return left;
         }
         return nullptr;
